af04e1839deadb984e10493e7399b95a71002c86
Andrii Kurdiumov <kant2002@gmail.com>
2025-09-21 22:54:47 +0500

    Add tests setup

---
+++ test/cleanup.test.js
@@ -0,0 +1,13 @@
+import { strictEqual } from 'assert';
+
+describe('Cleanup Functionality', () => {
+    it('should remove temporary files', () => {
+        // Test case logic here
+        strictEqual(true, true);
+    });
+
+    it('should reset application state', () => {
+        // Test case logic here
+        strictEqual(true, true);
+    });
+});
